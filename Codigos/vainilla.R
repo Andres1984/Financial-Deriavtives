c09850f2def23f930b1102561fa77aa77ce17710
Andrés <andres.marfh@gmail.com>
2020-09-16 11:05:05 -0500

    vainilla

--- Codigos/vainilla.R
+++ Codigos/vainilla.R
@@ -90,4 +90,54 @@ opput<-function(S,K){
 }
 
 
+## Bull spread
 
+### Necesitamos un call en largo precio strike K1
+### y una call en corto precio strike K2 para K1<K2
+
+K1=40
+K2=60
+S=20:100
+
+call=opcall(S,K1)
+scall=sopcall(S,K2)
+
+bull=scall+call
+
+plot(S,bull,col="green", type="l", ylim = c(-40,40), main="Bull Spread",lwd=8,ylab="Pay Off", xlab="Precio")
+lines(S,scall,col="red",type="l")
+lines(S,call,col="blue",type="l")
+
+
+### Bear spread
+
+
+### Necesitamos una opción put en largo con precio strike K2
+### Necestiamos una opción put en corto con precio strike K1
+
+put=opput(S,K2) 
+sput=sopput(S,K1)
+
+bear=sput+put
+
+plot(S,bear,col="green", type="l", ylim = c(-20,40), main="Bear Spread",lwd=8,ylab="Pay Off", xlab="Precio")
+lines(S,sput,col="red",type="l")
+lines(S,put,col="blue",type="l")
+
+
+### Strangle
+
+
+
+## Put en largo
+## Call en largo
+
+
+call=opcall(S,K2)
+put=opput(S,K1) 
+
+strang=call+put
+
+plot(S,strang,col="green", type="l", ylim = c(-20,40), main="Strangle Spread",lwd=8,ylab="Pay Off", xlab="Precio")
+lines(S,put,col="red",type="l")
+lines(S,call,col="blue",type="l")
