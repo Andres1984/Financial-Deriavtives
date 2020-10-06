bf0ec90a4126f0995cbec1ed8e7725d833ef2598
Andrés <andres.marfh@gmail.com>
2020-10-06 15:53:57 -0500

    Binomial

--- Codigos/OpcionesBinomial.R
+++ Codigos/OpcionesBinomial.R
@@ -15,7 +15,7 @@ library(IntroCompFinR)
 
 ### primer usamos quantmod
 simbolos=c("TSLA","XOM","JPM")
-getSymbols(simbolos,src="yahoo", from="2019-09-30",to="2020-09-30")
+getSymbols(simbolos,src="yahoo", from="2019-09-30",to="2020-10-06")
 #### Estrategia 
 #### Vamos a usar los pesos de un portafolio para decidir 
 #### Si compramos opciones call o put
@@ -43,5 +43,59 @@ rownames(weights)=simbolos
 colnames(weights)="weights"
 
 
+### Valoración opciones
+
+r=0.14813/100 ## Porcentaje anual
+rs=(1+r)^(1/52)-1## Tasa semanal
+
+### TSLA ## Prima de mercado 36.35
+
+ST=429
+KT=450
+
+volT=sd(tsla)*sqrt(5)## Proyecciones semanales
+tt=4
+nstep=4
+d=0
+## Proyección del precio de TSLA
+## Usar la libreria  derivmkts 
+## crr Cox Ross Rubinstein
+
+binomplot(ST, 0, volT, rs, tt, d, nstep,pointsize =0, crr = TRUE, plotvalues = TRUE,plotarrows = TRUE,titles = FALSE)
+
+### Graficar la opción Call
+binomplot(ST, KT, volT, rs, tt, d,nstep,american = TRUE, crr = TRUE, plotvalues = TRUE, plotarrows = TRUE, titles=TRUE)
+
+
+## CalculR
+a=binomopt(ST, KT, volT, rs, tt, d, nstep, american=TRUE, putopt=FALSE, crr = TRUE,returntrees=TRUE, returnparams=TRUE)
+a$price
+
+
+
+### Proyección Precios XOM
+### Calcular el precio de una opción
+
+STXOM=34.10
+KXOM=32.00
+KXOM1=30.00
+
+
+volTXOM=sd(xom)*sqrt(5)## Proyecciones semanales
+tt=4
+nstep=4
+d=0
+
+binomplot(STXOM, 0, volTXOM, rs, tt, d, nstep,pointsize =0, crr = TRUE, plotvalues = TRUE,plotarrows = TRUE,titles = FALSE)
+
+### Graficar la opción Put
+binomplot(STXOM, KXOM, volTXOM, rs, tt, d,nstep,american = TRUE, putopt=TRUE,crr = TRUE, plotvalues = TRUE, plotarrows = TRUE, titles=TRUE)
+
+
+## CalculR
+b=binomopt(STXOM, KXOM, volTXOM, rs, tt, d, nstep, american=TRUE, putopt=TRUE, crr = TRUE,returntrees=TRUE, returnparams=TRUE)
+b$price
+b1=binomopt(STXOM, KXOM1, volTXOM, rs, tt, d, nstep, american=TRUE, putopt=TRUE, crr = TRUE,returntrees=TRUE, returnparams=TRUE)
+b1$price
 
 
